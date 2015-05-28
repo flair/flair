ffbc06a8a3793810ffb52a2ccc24d135cb09c2a0
Justin Walsh <thejustinwalsh@users.noreply.github.com>
2015-05-27 17:56:53 -0700

    Linux Support
    Updated premake script to build on linux

--- premake.lua
+++ premake.lua
@@ -5,6 +5,8 @@ solution "flair"
          ["CLANG_CXX_LANGUAGE_STANDARD"] = "c++11",
          ["CLANG_CXX_LIBRARY"] = "libc++"
       }
+   filter { "action:gmake*" }
+      buildoptions { "-std=c++11" }
 
 project "flair"
    kind "StaticLib"
@@ -33,6 +35,8 @@ project "tests"
    files { "tests/**.cc", "vendor/googletest/src/gtest_main.cc", "vendor/googletest/src/gtest-all.cc" }
 
    links "flair"
+   filter { "action:gmake*" }
+      links "pthread"
 
    filter "configurations:Debug"
       defines { "DEBUG" }
@@ -41,3 +45,6 @@ project "tests"
    filter "configurations:Release"
       defines { "NDEBUG" }
       optimize "On"
+
+
+
